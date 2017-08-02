517966df4421dcaf62ec409d70b163fa22f2d578
Fred Snyder <fsnyder@castwide.com>
2017-08-02 02:20:59 -0400

    Verify gem installation and version.

--- src/commands.ts
+++ src/commands.ts
@@ -13,3 +13,14 @@ export function solargraphCommand(args:Array<String>, configuration:Configuratio
 	if (configuration.workspace) env['cwd'] = configuration.workspace;
 	return child_process.spawn(cmd.shift(), cmd.concat(args), env);
 }
+
+export function gemCommand(args:Array<String>, configuration:Configuration):child_process.ChildProcess {
+	let cmd = [];
+	if (configuration.useBundler && configuration.workspace) {
+		cmd.push('bundle', 'exec');
+	}
+	cmd.push('gem');
+	var env = { shell: true };
+	if (configuration.workspace) env['cwd'] = configuration.workspace;
+	return child_process.spawn(cmd.shift(), cmd.concat(args), env);
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import * as cmd from './commands';
 import * as path from 'path';
 import * as fs from 'fs';
 
+console.log('yeah, yer here');
+
 export {
     Configuration,
     Server
@@ -29,15 +31,32 @@ export function nearestWorkspace(file:string, parent?:string):string {
 	return result;
 }
 
-export function testGem(workspace:String, configuration:Configuration):Promise<Boolean> {
-    return new Promise((resolve) => {
+export function verifyGemIsInstalled(configuration:Configuration):Promise<Boolean> {
+    return new Promise((resolve, reject) => {
         var solargraphTest = cmd.solargraphCommand(['help'], configuration);
         solargraphTest.on('exit', (code) => {
             if (code == 0) {
                 resolve(true);
             } else {
-                resolve(false);
+                reject(false);
             }
         });
     });
+}
+
+export function verifyGemIsCurrent(configuration:Configuration):Promise<Boolean> {
+    return new Promise((resolve, reject) => {
+		let child = cmd.gemCommand(['outdated'], configuration);
+		let result = "\n";
+		child.stdout.on('data', (data:Buffer) => {
+			result += data.toString();
+		});
+		child.on('exit', () => {
+			if (result.match(/[\s]solargraph[\s]/)) {
+				reject(false);
+			} else {
+				resolve(true);
+			}
+		});
+	});
 }
